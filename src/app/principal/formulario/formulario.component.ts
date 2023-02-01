ca741d53413cf172fb31dded0667f5365d2dcc36
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-01-31 19:33:59 -0500

    form normal

--- src/app/principal/formulario/formulario.component.ts
+++ src/app/principal/formulario/formulario.component.ts
@@ -5,19 +5,43 @@ import { UserservicesService } from 'src/app/services/userservices.service';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {
+  //Crear instancia de UserServices
+  constructor(private userService: UserservicesService) {}
 
-    //Crear instancia de UserServices
-    constructor(private userService: UserservicesService){}
+  // Crear objeto que se evniara a al api
+  datos = { nombre: '', email: '', mensaje: '' };
+  errorNombre = '';
+  errorEmail = '';
+  errorMensaje = '';
 
-    // Crear objeto que se evniara a al api
-    datos: Users = {id: '', name: '', username:'', email:'', phone:'', website:''};
-
-    onSubmit(){
-    this.userService.postUser(this.datos).subscribe(
-    (usuario: Users)=>console.log(usuario)
-    );
+  // funacion de validar formulario retorna true si es valido
+  validarFormulario() {
+    let valido = true;
+    if (this.datos.nombre == '') {
+      this.errorNombre = 'El nombre es requerido';
+      valido = false;
+    } else {
+      this.errorNombre = '';
     }
+    if (this.datos.email == '') {
+      this.errorEmail = 'El email es requerido';
+      valido = false;
+    } else {
+      this.errorEmail = '';
+    }
+    if (this.datos.mensaje == '') {
+      this.errorMensaje = 'El mensaje es requerido';
+      valido = false;
+    } else {
+      this.errorMensaje = '';
+    }
+    return valido;
+  }
+
+  enviarFormulario() {
+    console.log(this.datos);
+  }
 }
