380893bb3c4479cbdb23c2ff71aaa5c0a412be99
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-01-31 21:05:54 -0500

    ------login ----

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,18 +10,20 @@ import { LoginComponent } from './principal/login/login.component';
 import { VistaComponent } from './principal/vista/vista.component';
 
 const routes: Routes = [
-  {path: 'vista', component: VistaComponent},
-  {path: 'formulario', component: FormularioComponent, canActivate: [CanactivateGuard]},
-  {path: 'home', component: HomeComponent, canActivate: [CanactivateGuard]},
+  { path: 'vista', component: VistaComponent, canActivate: [CanactivateGuard] },
+  {
+    path: 'formulario',
+    component: FormularioComponent,
+    canActivate: [CanactivateGuard],
+  },
+  { path: 'home', component: HomeComponent, canActivate: [CanactivateGuard] },
 
-  {path: '', component: LoginComponent },
-  {path: 'login', component: LoginComponent },
-
-
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/principal/login/login.component.ts
+++ src/app/principal/login/login.component.ts
@@ -5,18 +5,47 @@ import { AutenticacionService } from 'src/app/services/autenticacion.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
-  constructor(private autenticacion: AutenticacionService, private router: Router){}
+  user = '';
+  pass = '';
+  userInvalid = false;
+  passInvalid = false;
+  errorMessage = '';
+  validUser = [
+    {
+      user: 'admin',
+      pass: 'admin',
+    },
+  ];
+  constructor(
+    private autenticacion: AutenticacionService,
+    private router: Router
+  ) {}
 
   redireccion = '';
 
-  login(){
-  this.autenticacion.login();
-  this.redireccion = this.autenticacion.urlUsuarioIntentaAcceder;
-  this.autenticacion.urlUsuarioIntentaAcceder = '';
-  this.router.navigate([this.redireccion]);
+  login() {
+    this.errorMessage = '';
+    if (this.user === '' || this.pass === '') {
+      if (this.user === '') {
+        this.userInvalid = true;
+      } else {
+        this.userInvalid = false;
+      }
+      this.errorMessage = 'Usuario y/o contraseña no pueden estar vacios';
+      return;
+    } else if (
+      this.validUser[0].user === this.user &&
+      this.validUser[0].pass === this.pass
+    ) {
+      this.autenticacion.login();
+      this.redireccion = this.autenticacion.urlUsuarioIntentaAcceder;
+      this.autenticacion.urlUsuarioIntentaAcceder = '';
+      this.router.navigate([this.redireccion]);
+    } else {
+      this.errorMessage = 'Usuario y/o contraseña incorrectos';
+    }
   }
 }
